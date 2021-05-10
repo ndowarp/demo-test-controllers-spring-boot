600010b8655d3d5d60ad788b5587473a6c07d240
ndowarp <nicolas961945@gmail.com>
2021-05-10 17:01:27 -0500

    Initial commit

---
+++ src/main/java/com/example/demotestcontrollersspringboot/DemoTestControllersSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotestcontrollersspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestControllersSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestControllersSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demotestcontrollersspringboot/DemoTestControllersSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotestcontrollersspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestControllersSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
