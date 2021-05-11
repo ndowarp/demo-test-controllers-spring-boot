2df9b262578b2679bb238dea26178282a75bab1d
ndowarp <nicolas961945@gmail.com>
2021-05-11 15:15:41 -0500

    Added Integration, Unit and Acceptance Tests
    Added Integration, Unit and Acceptance Tests for Welcome endpoint

--- src/test/java/com/example/demotestcontrollersspringboot/controllers/WelcomeControllerAcceptanceTest.java
+++ src/test/java/com/example/demotestcontrollersspringboot/controllers/WelcomeControllerAcceptanceTest.java
@@ -1,10 +1,9 @@
 package com.example.demotestcontrollersspringboot.controllers;
 
-import com.example.demotestcontrollersspringboot.service.WelcomeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -16,36 +15,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest
-class WelcomeControllerIntegrationTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+class WelcomeControllerAcceptanceTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private WelcomeService welcomeService;
-
     @Test
     void shouldGetDefaultWelcomeMessage() throws Exception {
-        when(welcomeService
-                .getWelcomeMessage("Stranger"))
-                .thenReturn("Welcome Stranger!");
         mockMvc.perform(get("/welcome"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Welcome Stranger!")));
-        verify(welcomeService).getWelcomeMessage("Stranger");
     }
 
     @Test
     void shouldGetCustomWelcomeMessage() throws Exception {
-        when(welcomeService
-                .getWelcomeMessage("Juan"))
-                .thenReturn("Welcome Juan!");
         mockMvc.perform(get("/welcome?name=Juan"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Welcome Juan!")));
-        verify(welcomeService).getWelcomeMessage("Juan");
     }
 }
